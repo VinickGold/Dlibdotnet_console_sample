7a00a358be82a8de2fa7cf63fe7d7e1dfcaf2631
Vinicius Goldenberg <vinicius.goldenberg@outlook.com>
2019-05-30 23:08:43 -0300

    Adding code sample

---
+++ testing_dlibdotnet/Program.cs
@@ -0,0 +1,30 @@
+﻿using DlibDotNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing_dlibdotnet
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "photo.jpg");
+
+			using (var faceDetector = Dlib.GetFrontalFaceDetector())
+			using (var img = Dlib.LoadImage<RgbPixel>(path))
+			{
+				Dlib.PyramidUp(img);
+
+				var detectedFaces = faceDetector.Operator(img);
+
+				Console.WriteLine($"Detected Faces: { detectedFaces.Count() }");
+
+				Console.ReadKey();
+			}
+		}
+	}
+}
